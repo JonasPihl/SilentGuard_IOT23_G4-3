1d61bc0e21c940e9110e3d60a59985b3e135bc36
Player-3 <66365357+Player-3@users.noreply.github.com>
2023-12-22 19:04:45 +0100

    added functionality to disable the face detection system via subprocessing

--- Python/flaskServer.py
+++ Python/flaskServer.py
@@ -1,25 +1,44 @@
 import os
-from flask import Flask, jsonify, send_file, send_from_directory
-
-import faceDet
+import subprocess
+from flask import Flask, jsonify, request, send_from_directory
 
 app = Flask(__name__)
+process = None
+running = False
+
+@app.route('/state_of_server')
+def state_of_server():
+    return jsonify(running)
 
 @app.route('/get_image_list')
 def get_image_list():
     image_files = [f for f in os.listdir("images") if f.endswith(".jpg")]
-    print(image_files)
     return jsonify({"image_list": image_files})
 
 @app.route('/images/<path:filename>')
 def serve_image(filename):
     return send_from_directory("images", filename)
 
-@app.route('/video')
-def get_video():
-    #cap = faceDet.cap
-    return faceDet.cap.read()
+#@app.route('/video')
+#def get_feed():
+#    call on the subprocess to get the feed
 
+@app.route('/on_off', methods=['POST'])
+def on_off():
+    global process, running
+    try:
+        status_request = request.args.get('value')
+        if status_request is not None:
+            if status_request == "true":
+                running = True
+                process = subprocess.Popen(["python3", "faceDet.py"])
+            else:
+                running = False
+                process.terminate()
+        return jsonify({"message": "Success"})
+    except Exception as e:
+        # Log the exception or handle it as needed
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
