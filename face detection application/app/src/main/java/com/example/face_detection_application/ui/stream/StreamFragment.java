e2f1b989d83510a0f81d63fdfbb6a3d678782a8b
JonasPihl <122288616+JonasPihl@users.noreply.github.com>
2023-12-23 13:04:56 +0100

    Added StreamView

--- face detection application/app/src/main/java/com/example/face_detection_application/ui/stream/StreamFragment.java
+++ face detection application/app/src/main/java/com/example/face_detection_application/ui/stream/StreamFragment.java
@@ -1,14 +1,19 @@
 package com.example.face_detection_application.ui.stream;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.VideoView;
+
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
 
 import com.example.face_detection_application.databinding.FragmentStreamBinding;
 
@@ -26,7 +31,36 @@ public class StreamFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         streamViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        super.onCreate(savedInstanceState);
+        // setContentView(binding.activity);
+
+        VideoView videoView = binding.videoView;
+        String videoUrl = "http://raspberry_pi_ip:5000/video";  // Replace with your Raspberry Pi's IP
+
+        VideoStreamTask videoStreamTask = new VideoStreamTask(videoView);
+        videoStreamTask.execute(videoUrl);
+
         return root;
+    }
+
+    private static class VideoStreamTask extends AsyncTask<String, Void, String> {
+        private final VideoView videoView;
+
+        public VideoStreamTask(VideoView videoView) {
+            this.videoView = videoView;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String videoUrl = params[0];
+            return params[0];
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            videoView.setVideoPath(result);
+            videoView.start();
+        }
     }
 
     @Override
