7f5580f747394737bc30dbde44858cbc400019a6
Player-3 <66365357+Player-3@users.noreply.github.com>
2023-12-27 14:01:59 +0100

    added code to connect to server for push notifications

--- face detection application/app/src/main/java/com/example/face_detection_application/MainActivity.java
+++ face detection application/app/src/main/java/com/example/face_detection_application/MainActivity.java
@@ -12,9 +12,18 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.face_detection_application.databinding.ActivityMainBinding;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private static final String serverAdress = "http://192.168.1.174:5000/connect";  // TODO Replace with Pi's IP    private OkHttpClient client;
+    private OkHttpClient client;
+    private WebSocket webSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,32 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(serverAdress).build();
+
+        webSocket = client.newWebSocket(request, new WebSocketListener() {
+
+            @Override
+            public void onMessage(WebSocket webSocket, String text) {
+                System.out.println("recived psu notification" + text);
+            }
+
+            @Override
+            public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+                super.onFailure(webSocket, t, response);
+            }
+        });
+
+
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        if (webSocket != null){
+            webSocket.close(1000, "Activcity closed");
+        }
     }
 
 
