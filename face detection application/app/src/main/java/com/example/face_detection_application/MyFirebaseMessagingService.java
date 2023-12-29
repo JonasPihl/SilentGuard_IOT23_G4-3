6dac9b3071f513e16173cd4e3ad9f9a6c479e776
JonasPihl <122288616+JonasPihl@users.noreply.github.com>
2023-12-29 10:54:36 +0100

    Notifications now work both inside and outside the app

--- face detection application/app/src/main/java/com/example/face_detection_application/MyFirebaseMessagingService.java
+++ face detection application/app/src/main/java/com/example/face_detection_application/MyFirebaseMessagingService.java
@@ -1,5 +1,14 @@
 package com.example.face_detection_application;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.graphics.drawable.IconCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -19,6 +28,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
             if (remoteMessage.getNotification() != null) {
                 // Handle notification payload
+
                 sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
             }
         }
@@ -26,6 +36,44 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         private void sendNotification(String title, String body) {
             // Customize this method to create and show a notification based on the title and body
             // You can use NotificationCompat.Builder to create a notification
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            String NOTIFICATION_CHANNEL_ID = "12345";
+
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    "My Notifications",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+
+
+            notificationChannel.setDescription("Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+
+            int smallIconResourceId = R.drawable.ic_notifications_black_24dp;
+            notificationBuilder.setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setTicker("Hearty365")
+                    .setSmallIcon(smallIconResourceId)
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setContentInfo("Info");
+
+            notificationManager.notify(12345, notificationBuilder.build());
+
+
+        }
+
+        @Override
+        public void onNewToken(String token) {
+            // Handle token refresh or retrieval here
         }
     }
 
